6e57eb90b02236a518e4c728cde71dfe99699d73
Metaldetect <stas.klymenko@icloud.com>
2023-05-28 21:34:09 +0200

    Update js video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,23 @@
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
 
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 const vimeoPlayer = new Player(iframe, {
   title: true,
   autoplay: true,
   loop: false,
   fullscreen: true,
-  quality: "1080p",
+  quality: '1080p',
 });
 
-const time_key = "videoplayer-current-time";
+const time_key = 'videoplayer-current-time';
 
 const getCurrentTime = function (currentTime) {
   const seconds = currentTime.seconds;
   localStorage.setItem(time_key, JSON.stringify(seconds));
 };
 
-vimeoPlayer.on("timeupdate", getCurrentTime);
-
-const handlePlay = throttle(function (data) {
-  const currentTime = data.seconds;
-  localStorage.setItem(time_key, JSON.stringify(currentTime));
-}, 1000);
-
-vimeoPlayer.on("play", handlePlay);
+vimeoPlayer.on('timeupdate', throttle(getCurrentTime, 1000));
 
 vimeoPlayer.setCurrentTime(JSON.parse(localStorage.getItem(time_key)) || 0);
